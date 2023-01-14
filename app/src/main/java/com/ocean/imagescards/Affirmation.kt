2db073426a39584cad231e0bd138831fa3993892
Pragyanshree Das <shree.pragyan23@gmail.com>
2023-01-14 19:24:12 +0530

    affirmation to be set...

---
+++ app/src/main/java/com/ocean/imagescards/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.ocean.imagescards
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
